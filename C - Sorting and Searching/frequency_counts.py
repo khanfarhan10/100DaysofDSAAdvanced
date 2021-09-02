fa715b5c67e00456879fbc1a8ef3b38fa3d8124e
Farhan Hai Khan <njrfarhandasilva10@gmail.com>
2021-09-02 18:49:32 +0530

    Added Approach 2 for Frequency Count

--- C - Sorting and Searching/frequency_counts.py
+++ C - Sorting and Searching/frequency_counts.py
@@ -4,10 +4,18 @@ python frequency_counts.py
 
 Try not to use libraries
 Algorithm Efficiency : O(n)
-"""
 
+Different Approach to remove if statement:
+1. Insert All Chars as Frequency Nil (0)
+2. Perform Linear Traversal with Incremental Frequency
+3. Remove Nil Values from the Frequency Dict
+
+# Filter dictionary by keeping elements whose values are more than 0
+newDict = dict(filter(lambda elem: elem[1] > 0, old_dict.items()))
+"""
 input_text = "FarhanHaiKhan"
-sample_space = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+
+# APPROACH 1
 
 char_frequency = {} # or dict
 
@@ -19,3 +27,24 @@ for each_letter in input_text:
 
 sorted_char_frequency = {k: v for k, v in sorted(x.items(), key=lambda item: item[0])}
 print(sorted_char_frequency)
+
+
+print("----------------------")
+
+# APPROACH 2
+
+sample_space = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+
+char_frequency = {} # or dict
+for each_letter in sample_space:
+    char_frequency[each_letter] = 0
+# CONSTANT ORDER (O(2*26))
+
+
+for each_letter in input_text:
+    char_frequency[each_letter] = char_frequency[each_letter] + 1
+# LINEAR ORDER (O(n))
+
+filtered_char_frequency = dict(filter(lambda elem: elem[1] > 0, char_frequency.items()))
+print(filtered_char_frequency)
+# CONSTANT ORDER (O(2*26))
